a232b776ff9281d312baab0d9cacd58001b93110
AgostinaAlvarez <agosalvarezz1999@gmail.com>
2025-12-27 13:09:09 -0300

    changes

--- src/components/StoreDetailsComponent.tsx
+++ src/components/StoreDetailsComponent.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 const StoreDetailsComponent: React.FC = () => {
   return (
     <>
+      {/* ================= HERO ================= */}
       <section aria-labelledby="hero-section">
         <h1 id="hero-section">Store Name</h1>
       </section>
+      {/* ================= HERO ================= */}
     </>
   );
 };
