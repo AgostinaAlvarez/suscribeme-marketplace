49b5a1baba1312ffc0c2aa9c0340d252db928fe8
AgostinaAlvarez <agosalvarezz1999@gmail.com>
2026-01-12 10:47:34 -0300

    changes

--- src/components/PlanDetailsComponent.tsx
+++ src/components/PlanDetailsComponent.tsx
@@ -94,12 +94,15 @@ const PlanDetailsComponent: React.FC = () => {
               A Casual Guide for Artificial Intelligence, Deep Learning, and
               Python Programmers
             </p>
-            <div className="plan-detail-hero-content-row">
+            <div className="plan-detail-hero-content-row plan-details-hero-props">
               <div className="plan-detail-hero-content-tag">
                 <span>Highest Rated</span>
               </div>
               <span className="plan-detail-hero-rating">
                 3.5 ★★★★★ <a href="/">{`(1,867 ratings)`}</a>
+              </span>
+              <span className="plan-detail-hero-rating-responsive">
+                3.5 ★ <a href="/" style={{ marginLeft: 3 }}>{`(1,867)`}</a>
               </span>
               <span>2,616 students</span>
             </div>
@@ -213,6 +216,58 @@ const PlanDetailsComponent: React.FC = () => {
                       <span>Product</span>
                     </div>
                   </div>
+                  {/*RESPONSIVE*/}
+                  <div className="plan-detail-articles-section-item-responsive-content">
+                    <div className="plan-detail-articles-section-item-responsive-details">
+                      <div className="plan-detail-articles-section-item-responsive-image"></div>
+                      <div className="plan-detail-articles-section-item-responsive-details-info">
+                        <h3>Product Name</h3>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor.
+                        </p>
+                        <div className="plan-detail-articles-section-item-info-row">
+                          <span>Propiedades:</span>
+                          <div className="plan-detail-articles-section-item-info-tag">
+                            <span>Color</span>
+                          </div>
+                          <div className="plan-detail-articles-section-item-info-tag">
+                            <span>Color</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="plan-detail-articles-section-item-responsive-configs-container">
+                      <div className="plan-detail-articles-section-item-info-row">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="11"
+                          height="11"
+                          fill="currentColor"
+                          className="bi bi-clock"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
+                          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
+                        </svg>
+                        <span>Recibiras este product 1 vez cada 1 mes</span>
+                      </div>
+                      <div className="plan-detail-articles-section-item-info-row">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="11"
+                          height="11"
+                          fill="currentColor"
+                          className="bi bi-truck"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5zm1.294 7.456A2 2 0 0 1 4.732 11h5.536a2 2 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456M12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2" />
+                        </svg>
+                        <span>Envio a domicilio</span>
+                      </div>
+                    </div>
+                  </div>
                 </article>
               </li>
             ))}
@@ -298,7 +353,7 @@ const PlanDetailsComponent: React.FC = () => {
         aria-labelledby="plan-detail-articles-section"
         className="plan-detail-section-container"
       >
-        <div className="plan-detail-seller-section-container package-detail-seller-section-content">
+        <div className=" package-detail-seller-section-content plan-detail-seller-section-container">
           <h2 id="articles-section" className="plan-detail-section-title">
             About the Store
           </h2>
