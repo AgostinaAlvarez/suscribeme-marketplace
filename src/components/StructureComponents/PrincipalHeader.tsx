3fdd4eaa2da7a11dd813fff2209e1c11dab120af
AgostinaAlvarez <agosalvarezz1999@gmail.com>
2026-01-12 00:06:00 -0300

    changes

--- src/components/StructureComponents/PrincipalHeader.tsx
+++ src/components/StructureComponents/PrincipalHeader.tsx
@@ -80,7 +80,7 @@ const PrincipalHeader: React.FC<ComponentProps> = ({
         </a>
         <img
           className="navbar-responsive-logo"
-          src="/assets/icons/bag-icon.svg"
+          src="/assets/icons/white-bag.svg"
           alt="Menu icon"
           width="14"
           height="14"
@@ -91,7 +91,7 @@ const PrincipalHeader: React.FC<ComponentProps> = ({
             style={{ border: 'none' }}
           >
             <img
-              src="/assets/icons/search.svg"
+              src="/assets/icons/search-custom.svg"
               alt="Search icon"
               width="14"
               height="14"
@@ -100,8 +100,17 @@ const PrincipalHeader: React.FC<ComponentProps> = ({
           </div>
           <span>Search for anything</span>
         </div>
+
         <img
+          className="navbar-responsive-menu"
           src="/assets/icons/menu.svg"
+          alt="Menu icon"
+          width="14"
+          height="14"
+        />
+        <img
+          className="navbar-mobile-menu"
+          src="/assets/icons/white-menu.svg"
           alt="Menu icon"
           width="14"
           height="14"
